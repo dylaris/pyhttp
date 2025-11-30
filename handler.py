538290cf91629b96a89de21775cbbc198837c0a1
Dylaris <3445818679@qq.com>
2025-11-30 21:36:32 +0800

    in windows, python use 'gbk' default, add encoding='utf-8' in handler

--- handler.py
+++ handler.py
@@ -8,20 +8,20 @@ class HTTPHandler:
         return HTTPResponse(404, "SOMETHING WRONG OCCURS!")
 
     def css(self, ctx):
-        with open("webroot/style.css", "r") as f:
+        with open("webroot/style.css", "r", encoding="utf-8") as f:
             return HTTPResponse(200, f.read(), "text/css")
 
     def home(self, ctx):
-        with open("webroot/home.html", "r") as f:
+        with open("webroot/home.html", "r", encoding="utf-8") as f:
             return HTTPResponse(200, f.read())
 
     def blog(self, ctx):
-        with open("webroot/blog.html", "r") as f:
+        with open("webroot/blog.html", "r", encoding="utf-8") as f:
             return HTTPResponse(200, f.read())
 
     def login(self, ctx):
         if ctx["request"].method == "GET":
-            with open("webroot/login.html", "r") as f:
+            with open("webroot/login.html", "r", encoding="utf-8") as f:
                 html = f.read()
                 return HTTPResponse(200, html)
         elif ctx["request"].method == "POST":
@@ -29,9 +29,9 @@ class HTTPHandler:
             password = ctx["request"].get_form_data("password")
             if username == ctx["username"] and password == ctx["password"]:
                 ctx["login"] = True
-                with open("webroot/home.html", "r") as f:
+                with open("webroot/home.html", "r", encoding="utf-8") as f:
                     return HTTPResponse(200, f.read())
             else:
                 ctx["login"] = False
-                with open("webroot/failure.html", "r") as f:
+                with open("webroot/failure.html", "r", encoding="utf-8") as f:
                     return HTTPResponse(200, f.read())
